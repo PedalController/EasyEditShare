58bb88444731299142e7134fe6e9df394e27637f
SrMouraSilva <mateus.moura@hotmail.com>
2016-02-06 17:28:51 -0300

    Initial commit
    Signed-off-by: SrMouraSilva <mateus.moura@hotmail.com>

---
+++ Readme.md
@@ -0,0 +1,10 @@
+EasyEditShare
+=============
+
+A simple Zoom Edit&Share© variation/implementation for JVM (and all the Operational Systems with support)
+
+## Run
+
+Execute EasyEditShare.jar (DoubleClick or java -jar EasyEditShare.jar). Open when the real multistomp has gone be connected on PC.
+
+![Live example](https://github.com/PedalController/EasyEditShare/blob/master/example/Use_example.png?raw=true)
---
+++ src/br/com/srmourasilva/editshare/EasyEditSharePresenter.java
@@ -0,0 +1,109 @@
+package br.com.srmourasilva.editshare;
+
+import javax.sound.midi.MidiUnavailableException;
+
+import br.com.srmourasilva.architecture.exception.DeviceNotFoundException;
+import br.com.srmourasilva.domain.OnMultistompListener;
+import br.com.srmourasilva.domain.message.CommonCause;
+import br.com.srmourasilva.domain.message.Messages;
+import br.com.srmourasilva.domain.message.Messages.Message;
+import br.com.srmourasilva.domain.multistomp.Effect;
+import br.com.srmourasilva.multistomp.controller.PedalController;
+import br.com.srmourasilva.multistomp.controller.PedalControllerFactory;
+import br.com.srmourasilva.multistomp.zoom.gseries.ZoomGSeriesMessages;
+
+public class EasyEditSharePresenter implements OnMultistompListener {
+
+	private EasyEditShare view;
+
+	private PedalController pedal;
+
+	public EasyEditSharePresenter(EasyEditShare view) {
+		this.view = view;
+	}
+
+	public void start() {
+		try {
+			this.pedal = PedalControllerFactory.searchPedal();
+		} catch (DeviceNotFoundException e) {
+			view.setTitle("Pedal not found! You connected any?");
+			return;
+		}
+
+		pedal.addListener(this);
+
+		try {
+			pedal.on();
+			
+		} catch (MidiUnavailableException e) {
+			view.setTitle("This Pedal has been used by other process program");
+			return;
+		}
+
+		pedal.send(ZoomGSeriesMessages.REQUEST_CURRENT_PATCH_NUMBER());
+	}
+
+	public void stop() {
+		if (pedal != null)
+			pedal.off();
+	}
+
+	@Override
+	public void onChange(Messages messages) {
+		messages.getBy(CommonCause.EFFECT_ACTIVE).forEach(message -> updateEffect(message, CommonCause.EFFECT_ACTIVE));
+		messages.getBy(CommonCause.EFFECT_DISABLE).forEach(message -> updateEffect(message, CommonCause.EFFECT_DISABLE));
+
+		messages.getBy(CommonCause.TO_PATCH).forEach(message -> setPatch(message));
+		messages.getBy(CommonCause.PATCH_NAME).forEach(message -> updateTitle((String) message.details().value));
+		
+		messages.getBy(CommonCause.PARAM_VALUE).forEach(message -> System.out.println(message));
+
+		messages.getBy(CommonCause.EFFECT_TYPE).forEach(message -> updateEffect(message, CommonCause.EFFECT_TYPE));
+	}
+
+	private void updateEffect(Message message, CommonCause cause) {
+		int patch  = message.details().patch;
+		int effect = message.details().effect;
+
+		boolean otherPatch = patch != pedal.multistomp().getIdCurrentPatch();
+		if (otherPatch)
+			return;
+
+		if (cause == CommonCause.EFFECT_ACTIVE)
+			view.active(effect);
+		else if (cause == CommonCause.EFFECT_DISABLE)
+			view.disable(effect);
+
+		else if (cause == CommonCause.EFFECT_TYPE)
+			view.setPedalName(effect, pedal.multistomp().currentPatch().effects().get(effect).getName());
+	}
+	
+	private void setPatch(Message message) {
+		int idPatch = message.details().patch;
+
+		pedal.send(ZoomGSeriesMessages.REQUEST_SPECIFIC_PATCH_DETAILS(idPatch));
+	}
+
+	private void updateTitle(String newTitle) {
+		view.setTitle(newTitle);
+	}
+
+
+	public void toogleEffectOf(int effect) {
+		this.pedal.toogleEffect(effect);
+	}
+
+	public void updateNameOf(int indexEffect, Effect effect) {
+		this.view.setPedalName(indexEffect, effect.getName());
+	}
+	
+	/////////////////////////////////////////////////////
+
+	public void nextPatch() {
+		this.pedal.nextPatch();
+	}
+	
+	public void beforePatch() {
+		this.pedal.beforePatch();
+	}
+}
